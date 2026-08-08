c8a434bee7f4d01bcfa675557cae8683068c0e73
Debshishu Ghosh <nawabdeb@gmail.com>
2026-08-08 20:18:45 +0530

    new build

--- lib/app/loading.jsx
+++
@@ -1,9 +0,0 @@
-export default function RootLoading() {
-  return (
-    <main className="landing">
-      <div className="landing-inner" style={{ opacity: 0.4 }}>
-        <h1>Nexus</h1>
-      </div>
-    </main>
-  );
-}
