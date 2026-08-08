75672c59e760f899f4323c956d80ee25096c723a
Debshishu Ghosh <nawabdeb@gmail.com>
2026-08-08 20:06:30 +0530

    .. login removed

--- app/api/auth/logout/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { CARE_COOKIE, MY_COOKIE } from '@/lib/auth';
-
-export async function POST(request) {
-  const { app } = await request.json().catch(() => ({}));
-  const res = NextResponse.json({ ok: true });
-  res.cookies.set(app === 'my' ? MY_COOKIE : CARE_COOKIE, '', { path: '/', maxAge: 0 });
-  return res;
-}
